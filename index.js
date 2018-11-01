f7c023f7172a86b9c87840a9b88815224738cc9f
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2018-11-01 11:58:23 +0100

    Insert sequentially and use tx for performance. Add router to bulk insert records to show performance of sync

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const db = (function() {
                 return Promise.resolve({
                     rows: []
                 })
-            }
+            },
+            'end': () => {}
         }
     }
 })()
@@ -37,7 +38,7 @@ app.set('view engine', 'handlebars')
 /**
  * Simple GET route to list accounts.
  */
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     const formatDate = (date) => {
         let m = date && date['diff'] ? date : date ? moment(date) : moment()
         return m.tz(process.env.TIMEZONE || 'Europe/Copenhagen').format(process.env.DATETIME_FORMAT || 'YYYY-M-D @ k:mm')
@@ -50,6 +51,52 @@ app.get('/*', (req, res) => {
 		res.render('accountlist', context)
     }).catch(err => {
         res.status(500).send(`Unable to get response from database (${err.message}) - did you create a Heroku Connect Mapping yet?`).end()
+    })
+})
+
+/**
+ * Add many certifications to the database.
+ */
+app.get('/addcertifications', (req, res) => {
+    // get account number for bulk insert
+    const accountNo = process.env.BULK_ACCOUNT_NO || 'CD736025'
+    const numRecords = (function() {
+        if (process.env.BULK_NUM_RECORDS) return process.env.BULK_NUM_RECORDS - 0
+        return 100
+    })()
+    console.log(`Inserting bulk certifications - ${numRecords} for ${accountNo}`)
+
+    // delete all rows from cerfifications (also to ensure we have the certification table)
+    return db.query('delete from salesforce.certification__c;').then(rs => {
+        return db.query('BEGIN;')
+    }).then(() => {
+        return db.query(`select sfid from salesforce.account where accountnumber='${accountNo}';`)
+    }).then(rs => {
+        // get the account id
+        const accountId = rs.rows[0].sfid
+        console.log(`Inserting ${numRecords} records for account with ID ${accountId}`)
+
+        // insert the data using sequential promises as it otherwise only took around 70
+        const promiseSerial = funcs =>
+            funcs.reduce((promise, func) =>
+            promise.then(result => func().then(Array.prototype.concat.bind(result))),
+            Promise.resolve([]))
+        return promiseSerial([...Array(numRecords).keys()].map(i => {
+            return () => db.query("insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) values ($1, now(), $2, $3);", 
+            [`d8b1ee8f-dfad-45e5-bfbf-counter${i}`, `Advanced Platform (${i})`, accountId])
+        }))
+    }).then(() => {
+        console.log('Committing...')
+        return db.query('COMMIT;')
+    }).then(() => {
+        console.log('Redirecting back to /...')
+        return res.redirect('/')
+    }).catch(err => {
+        // error - table probably not there - send to error page
+        console.log(err)
+        db.query('ROLLBACK;').then(rs => {
+            res.render('nocertificationtable')
+        })
     })
 })
 
--- sql/create_certification.sql
+++ sql/create_certification.sql
@@ -1,3 +1,15 @@
 insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) 
     select 'd8b1ee8f-dfad-45e5-bfbf-f0e28daf25de', now(), 'Advanced Sales', 
     sfid from salesforce.account where accountnumber='CC978213';
+insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) 
+    select 'd8b1ee8f-dfad-45e5-bfbf-f0e28daf25df', now(), 'Advanced Service', 
+    sfid from salesforce.account where accountnumber='CC978213';
+insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) 
+    select 'd8b1ee8f-dfad-45e5-bfbf-f0e28daf25dg', now(), 'Advanced Commerce', 
+    sfid from salesforce.account where accountnumber='CC978213';
+insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) 
+    select 'd8b1ee8f-dfad-45e5-bfbf-f0e28daf25dh', now(), 'Advanced Marketing', 
+    sfid from salesforce.account where accountnumber='CC978213';
+insert into salesforce.certification__c (external_id__c, date_issued__c, name, account__c) 
+    select 'd8b1ee8f-dfad-45e5-bfbf-f0e28daf25di', now(), 'Advanced Platform', 
+    sfid from salesforce.account where accountnumber='CC978213';
